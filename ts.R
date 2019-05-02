1f58c2f113964206947e00f9113683c74e2a13f8
josephsdavid <josephsd@smu.edu>
2019-05-01 23:57:54 -0500

    added R function for testing stationarity condition 3

--- ts.R
+++ ts.R
@@ -13,3 +13,17 @@ tplot  <- function(vec, begin, end){
 # Realization1 = gen.arma.wge(n = 250)
 #
 # tplot(Realization1, 1, 250)
+
+# Split ACF plot to check for independence of correlation (requirement 3)
+
+#requires(nothing)
+
+splitacf = function(vec){
+	par(mfrow = c(1,3))
+	acf(vec)
+	acf(vec[1:(length(vec)/2)])
+	acf(vec[(length(vec)/2):length(vec)])
+}
+
+# Realize = gen.arma.wge(500,0.95,0,plot = T, sn = 784)
+# splitacf(Realize)
