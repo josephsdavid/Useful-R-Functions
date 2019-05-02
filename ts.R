2cb040d76dc2e1ed2828587dde1b47c0c7a36d60
josephsdavid <josephsd@smu.edu>
2019-05-02 01:12:13 -0500

    added vxbar function from unit 1.9 (will be fixed if we decide it is broken)

--- ts.R
+++ ts.R
@@ -28,3 +28,31 @@ splitacf = function(vec){
 
 # Realize = gen.arma.wge(500,0.95,0,plot = T, sn = 784)
 # splitacf(Realize)
+
+# vxbar: variance of a stationary time series where autocorrelation approaches zero (from section 1.9) 
+# requires:
+
+library(purrr)
+library(dplyr)
+
+# Code
+vxbar  <- function(vec){
+	vec %>%
+		discard(is.na) -> vec
+	n  <-  length(vec)
+	nlag  <-  n-1
+	m  <- mean(vec)
+	v  <-  var(vec)
+	g0  <- v*nlag/n
+	aut  <-  acf(vec,lag.max = nlag)
+	sum  <- 0
+	for (k in 1:nlag){
+		sum  <- sum + (1-k/n)*aut$acf[k+1]*g0
+	}
+	 return(2*sum/n + g0/n)
+}
+
+# example
+# brate = read.csv("../../Data/10_year_bond_rate_2010-2015.csv")
+# x <- brate$Adj.Close
+# vxbar(x)
